339dd5636a70945f6aa38fc2f6b51d2fc5d3fa3f
Kotyga <kotmayyaka@gmail.com>
2024-04-14 22:13:11 +0300

    add сравнение по отраслям

--- home.py
+++ home.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from data import load_data, inflation
+import plotly.express as px
 
 query = '''select name, year, mean_salary
 from data.salary s
@@ -16,14 +17,14 @@ query1 = '''select name, s.year, mean_salary, total
 from data.salary s
 inner join data.ref_salary rs on rs.id = s.economic_id
 inner join data.inflation_data_0 id0 on id0.year = s.year'''
-df = load_data(query0)
+df11 = load_data(query0)
 df1 = load_data(query1)
 st.title('''Таблица уровня инфляции по месяцам в годовом исчислении''')
 st.write('''Коэффициент инфляции в годовом исчислении,
          представленный в таблице ниже, рассчитывается как сумма коэффициентов инфляции 
          за 12 месяцев, включая выбранный. Такой способ позволяет оценить динамику изменения 
          уровня инфляции в целом, сглаживая сезонные отклонения.)''')
-st.dataframe(df)
+st.dataframe(df11)
 st.markdown('[Source](https://уровень-инфляции.рф/таблицы-инфляции)')
 df1['salary_gen'] = df1.apply(lambda row: inflation(row['mean_salary'], row['total']), axis=1)
 st.line_chart(df1, x='year', y='salary_gen', color="name")
@@ -33,12 +34,28 @@ query1 = '''select name, s.year, mean_salary, total
 from data.salary s
 inner join data.ref_salary rs on rs.id = s.economic_id
 inner join data.inflation_data_1 id0 on id0.year = s.year'''
-df = load_data(query0)
+df11 = load_data(query0)
 df1 = load_data(query1)
 st.title('''Таблица уровня инфляции по месяцам в годовом исчислении''')
 st.write('''В таблице ниже представлен уровень инфляции 
          за каждый месяц в отдельности, с первое по последнее число месяца.''')
-st.dataframe(df)
+st.dataframe(df11)
 st.markdown('[Source](https://уровень-инфляции.рф/таблицы-инфляции)')
 df1['salary_rel'] = df1.apply(lambda row: inflation(row['mean_salary'], row['total']), axis=1)
 st.line_chart(df1, x='year', y='salary_rel', color="name")
+
+choice = st.selectbox(
+    'Выберите отрасль:',
+    ('Финансовая деятельность', 'Образование', 'Здравоохранение и предоставление социальных услуг')
+)
+
+# Строим график
+fig = px.line()
+d = df[df['name']==choice]
+dd = df1[df1['name']==choice]
+# Строим график
+fig = px.line(d, x="year", y="mean_salary")
+fig.add_scatter(x=dd["year"], y=dd["salary_rel"], mode="lines")
+
+# Отображаем график
+st.plotly_chart(fig)
